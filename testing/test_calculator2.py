1e332330ce84adb2caa9071b38548f88d56d3f1f
lixinxin <lixinxin@ktjr.com>
2020-06-20 04:01:34 +0800

    pytest(实战二) 作业 补充依赖关系

--- testing/test_calculator2.py
+++ testing/test_calculator2.py
@@ -13,24 +13,27 @@ cal = Calculator()
 
 class TestCal:
 
-    # 测试除法
-    @pytest.mark.run(order=4)
-    def test_div1(self):
-        assert 1 == cal.div(1, 1)
-
     # 测试乘法
+    @pytest.mark.dependency(name=["test_mul"])  # 创建依赖：test_mul
     @pytest.mark.run(order=3)
     def test_mul1(self):
         assert 1 == cal.mul(1, 1)
 
+    # 测试除法
+    @pytest.mark.dependency(depends=["test_mul"])  # 除法依赖乘法
+    @pytest.mark.run(order=4)
+    def test_div1(self):
+        assert 1 == cal.div(1, 1)
+
+    # 测试加法
+    @pytest.mark.dependency(name=["test_add"])  # 创建依赖：test_add
+    @pytest.mark.run(order=1)
+    @pytest.mark.parametrize("a,b,result", yaml.safe_load(open("./datas/datas.yaml")), ids=["整数", "浮点数", "负数"])
+    def test_add1(self, a, b, result):
+        assert result == cal.add(a, b)
+
     # 测试减法
+    @pytest.mark.dependency(depends=["test_add"])  # 减法依赖加法
     @pytest.mark.run(order=2)
     def test_sub1(self):
         assert 0 == cal.sub(1, 1)
-
-    # 测试加法
-    @pytest.mark.run(order=1)
-    @pytest.mark.parametrize("a,b,result", yaml.safe_load(open("./datas/datas.yaml")),
-                             ids=["整数", "浮点数", "负数"])
-    def test_add1(self, a, b, result):
-        assert result == cal.add(a, b)
